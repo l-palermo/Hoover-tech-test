90feb72187d67f4bd1a8fc6480990282a81d1f99
Luigi <l-palermo@hotmail.it>
2019-11-13 23:44:20 +0000

    dev: refactor InputParser

--- src/InputParser.js
+++ src/InputParser.js
@@ -1,38 +1,37 @@
-function InputParser(input) {
-  this.data = input;
-  this.Xmax = null;
-  this.Ymax = null;
-  this.hooverX = null;
-  this.hooverY = null;
-  this.directions = null;
-  this.patchLocation = null;
-  this.run();
-}
-
-InputParser.prototype = {
+class InputParser {
+  constructor(input) {
+    this.data = input;
+    this.Xmax = null;
+    this.Ymax = null;
+    this.hooverX = null;
+    this.hooverY = null;
+    this.directions = null;
+    this.patchLocation = null;
+    this.run();
+  }
 
   roomDimension() {
     const room = this.data.splice(0, 1).join();
     this.Xmax = Number(room[0]);
     this.Ymax = Number(room[2]);
-  },
+  }
 
   hooverLocation() {
     const hooverLocation = this.data.splice(0, 1).join();
     this.hooverX = Number(hooverLocation[0]);
     this.hooverY = Number(hooverLocation[2]);
-  },
+  }
 
   actions() {
     this.directions = this.data.pop().split('');
-  },
+  }
 
   run() {
     this.roomDimension();
     this.hooverLocation();
     this.actions();
     this.patchLocation = this.data;
-  },
-};
+  }
+}
 
 exports.InputParser = InputParser;
